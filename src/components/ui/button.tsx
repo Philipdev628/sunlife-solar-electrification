b11f884a11bda7987129d93758736c86f59e84b5
Philip <ireyinkadiamond@gmail.com>
2026-07-09 13:12:23 +0100

    Checkpoint before debugging button text issue

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -70,15 +70,23 @@ export interface ButtonProps
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, variant, size, fullWidthOnMobile, asChild = false, ...props }, ref) => {
     const Comp = asChild ? Slot : "button";
-    return (
+
+ return (
       <Comp
-        className={cn(buttonVariants({ variant, size, fullWidthOnMobile }), className)}
+        className={cn(
+          buttonVariants({
+            variant,
+            size,
+            fullWidthOnMobile,
+          }),
+          className
+        )}
         ref={ref}
         {...props}
       />
     );
   }
 );
-Button.displayName = "Button";
+
 
 export { Button, buttonVariants };
